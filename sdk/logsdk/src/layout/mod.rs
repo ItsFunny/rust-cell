58273eb622ec8888e5a7d13569a49f46bfd36b6b
itsfunny <18757883747@163.com>
2022-05-02 22:10:30 +0800

    refactor

--- sdk/logsdk/src/layout/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod layout {
-    use crate::hook::log_hook::LogEntry;
-
-    pub trait IEntryLayOut {
-        fn lay_out(e: &LogEntry) -> str;
-    }
-}
--- sdk/logsdk/src/module.rs
+++
@@ -1,8 +0,0 @@
-use std::fmt::{Display, Formatter, write};
-
-pub trait ModuleTrait{
-    fn index(&self) -> u16;
-    fn name(&self) -> String;
-}
-
-
