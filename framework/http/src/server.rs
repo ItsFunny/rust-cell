6cb9a90fdfba6ebb975a98d11f170e6393db73a2
itsfunny <18757883747@163.com>
2022-06-24 09:00:22 +0800

    server

--- framework/http/src/server.rs
+++ framework/http/src/server.rs
@@ -15,6 +15,7 @@ use cell_core::cerror::{CellError, CellResult, ErrorEnums, ErrorEnumsStruct};
 use cell_core::channel::ChannelTrait;
 use cell_core::dispatcher::{DefaultDispatcher, DispatchContext};
 use cell_core::request::MockRequest;
+use logsdk::{cerror, cinfo, module_enums};
 use crate::channel::HttpChannel;
 use crate::dispatcher::HttpDispatcher;
 use crate::request::HttpRequest;
@@ -22,8 +23,6 @@ use crate::response::HttpResponse;
 
 pub struct HttpServer {
     dispatcher: DefaultDispatcher<'static, 'static>,
-    // _marker_e: PhantomData<&'e ()>,
-    // _marker_a: PhantomData<&'a ()>,
 }
 
 impl Debug for HttpServer {
@@ -32,65 +31,13 @@ impl Debug for HttpServer {
     }
 }
 
+module_enums!(
+        (HTTP_SERVER,1,&logsdk::common::LogLevel::Info);
+    );
+
 unsafe impl Send for HttpServer {}
 
 unsafe impl Sync for HttpServer {}
-// impl<'static> Clone for HttpServer<'static>{
-//     fn clone(&self) -> Self {
-//         HttpServer{
-//             dispatcher: (),
-//             _marker_e: Default::default(),
-//             _marker_a: Default::default()
-//         }
-//     }
-// }
-
-// fn create_tcp_listener(addr: net::SocketAddr, backlog: u32) -> CellResult<net::TcpListener> {
-//     use socket2::{Domain, Protocol, Socket, Type};
-//     let domain = Domain::for_address(addr);
-//     let socket = Socket::new(domain, Type::STREAM, Some(Protocol::TCP))?;
-//     socket.set_reuse_address(true)?;
-//     socket.bind(&addr.into())?;
-//     // clamp backlog to max u32 that fits in i32 range
-//     let backlog = cmp::min(backlog, i32::MAX as u32) as i32;
-//     socket.listen(backlog)?;
-//     Ok(net::TcpListener::from(socket))
-// }
-
-// impl HttpServer{
-//     pub fn listen(mut self, lst: net::TcpListener) -> CellResult<Self> {
-//
-//     }
-// }
-//
-//
-
-// fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-//     let body = async {
-//         let addr = ([127, 0, 0, 1], 3000).into();
-//         let service = make_service_fn(|_conn: &AddrStream| {
-//             let a = _conn.clone();
-//             ::std::io::_print(::core::fmt::Arguments::new_v1(
-//                 &["", "\n"],
-//                 &[::core::fmt::ArgumentV1::new_debug(&a)],
-//             ));
-//             async move { Ok::<_, hyper::Error>(service_fn(echo)) }
-//         });
-//         let server = Server::bind(&addr).serve(service);
-//         ::std::io::_print(::core::fmt::Arguments::new_v1(
-//             &["Listening on http://", "\n"],
-//             &[::core::fmt::ArgumentV1::new_display(&addr)],
-//         ));
-//         server.await?;
-//         Ok(())
-//     };
-//     #[allow(clippy::expect_used)]
-//     tokio::runtime::Builder::new_multi_thread()
-//         .enable_all()
-//         .build()
-//         .expect("Failed building the Runtime")
-//         .block_on(body)
-// }
 
 
 impl HttpServer {
@@ -98,51 +45,11 @@ impl HttpServer {
         let addr = ([127, 0, 0, 1], 3000).into();
         let s1 = Arc::new(self);
         let service = make_service_fn(|_conn: &AddrStream| {
-            // let a = _conn.clone();
-            let addr=_conn.remote_addr();
-            //         service_fn(move |req| {
-            //             let http_req=HttpRequest::new(req);
-            // let http_resp=HttpResponse::new();
-            //             self.dispatch(http_req,http_resp);
-            //             // cell_hyper_service_fn(req)
-            //         });
+            let addr = _conn.remote_addr();
             let s2 = s1.clone();
             async move {
                 Ok::<_, hyper::Error>(service_fn(move |req| {
-                    // c()
                     async_hyper_service_fn(s2.clone(), req, addr)
-
-                    // let (tx, rx) = oneshot::channel();
-                    // let http_req = HttpRequest::new(req);
-                    // let http_resp = HttpResponse::new(tx);
-                    // let ctx = DispatchContext::new(Box::new(http_req), Box::new(http_resp));
-                    // self.dispatcher.dispatch(ctx);
-
-                    // async_hyper_service_fn(s2.clone(),req)
-
-                    // let (tx, rx) = oneshot::channel();
-                    // tokio::spawn(async move {
-                    //     let server=s2.clone();
-                    //     // hyper_service_fn(req, tx)
-                    //     rx.await.map_err(|e| io::Error::new(io::ErrorKind::BrokenPipe, e))
-                    // });
-
-
-                    // let (txx, mut rxx) = std::sync::mpsc::channel::<Response<Body>>();
-                    // let (tx, rx) = oneshot::channel();
-                    // tokio::spawn(async move {
-                    //     let http_req=HttpRequest::new(req);
-                    //     let http_resp=HttpResponse::new(tx);
-                    //     let ctx=DispatchContext::new(Box::new(http_req),Box::new(http_resp));
-                    //     self.dispatcher.dispatch(ctx);
-                    // });
-                    // // self.dispatcher.dispatch(ctx);
-                    // // ss.dispatch(http_req, http_resp);
-                    // // cell_hyper_service_fn(req)
-                    // // self.hyper_service_fn(req)
-                    // // rxx.recv()
-                    // rx.await.map_err(|e| io::Error::new(io::ErrorKind::BrokenPipe, e))
-                    // c()
                 }))
             }
         }
@@ -191,84 +98,27 @@ pub async fn async_hyper_service_fn(mut server: Arc<HttpServer>, req: Request<Bo
         let ret: ChannelWrapper;
         match rrr {
             Ok(v) => {
-                println!("success");
                 ret = ChannelWrapper { Err: None, Ret: Some(v) };
             }
             Err(e) => {
-                // let a=e;
-                println!("failed:{}", e);
                 ret = ChannelWrapper { Err: Some(e), Ret: None };
             }
         }
-        tx.send(ret);
+        tx.send(ret).unwrap();
     });
     let ret = rx.await.map_err(|e| io::Error::new(io::ErrorKind::BrokenPipe, e));
     match ret {
         Ok(v) => {
             if let Some(e) = v.Err {
-                // Err(v.Err.unwrap())
-                // Err(Error::from(io::ErrorKind::BrokenPipe))
+                cerror!(ModuleEnumsStruct::HTTP_SERVER,"调用失败:{}",e.to_string());
                 Err(e)
-                // Err(CellError::from(ErrorEnumsStruct::UNKNOWN))
-                // Err(io::Error::new(io::ErrorKind::BrokenPipe, e))
-                // Err(CellError::from(ErrorEnumsStruct::JSON_SERIALIZE))
             } else {
-                println!("success");
                 Ok(v.Ret.unwrap())
             }
         }
         Err(e) => {
-            println!("failed:{}", e);
+            cerror!(ModuleEnumsStruct::HTTP_SERVER,"调用失败:{}",e.to_string());
             Err(e)
-        }
-    }
-}
-
-
-async fn c() -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from(String::from("asd"))))
-}
-
-
-async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        // Serve some instructions at /
-        (&Method::GET, "/") => Ok(Response::new(Body::from(
-            "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d 'hello world'`",
-        ))),
-
-        // Simply echo the body back to the client.
-        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
-
-        // Convert to uppercase before sending back to client using a stream.
-        (&Method::POST, "/echo/uppercase") => {
-            let chunk_stream = req.into_body().map_ok(|chunk| {
-                chunk
-                    .iter()
-                    .map(|byte| byte.to_ascii_uppercase())
-                    .collect::<Vec<u8>>()
-            });
-            Ok(Response::new(Body::wrap_stream(chunk_stream)))
-        }
-
-        // Reverse the entire body before sending back to the client.
-        //
-        // Since we don't know the end yet, we can't simply stream
-        // the chunks as they arrive as we did with the above uppercase endpoint.
-        // So here we do `.await` on the future, waiting on concatenating the full body,
-        // then afterwards the content can be reversed. Only then can we return a `Response`.
-        (&Method::POST, "/echo/reversed") => {
-            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
-
-            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
-            Ok(Response::new(Body::from(reversed_body)))
-        }
-
-        // Return the 404 Not Found for other routes.
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
         }
     }
 }
